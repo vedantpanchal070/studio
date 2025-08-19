6b330e894be7fdce140d450fb2df79b84a184b4a
Vedant Panchal <vedantpanchal0009@gmail.com>
2025-08-19 04:53:20 +0000

    can u make this web app to software

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: false, // Always enable PWA features
 });
 
 const nextConfig: NextConfig = {
