c167e4d5f94c8b7766c8da9554d8e6630e8bad8f
Vedant Panchal <vedantpanchal0009@gmail.com>
2025-09-17 13:58:47 +0000

    how i can run this web app fully offline

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 
 import type { NextConfig } from 'next';
 
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -21,4 +28,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
